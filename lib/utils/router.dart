5bc64159340b3f8946b2c453aaaa40db07b37184
PedroBarr <pebarreraa@udistrital.edu.co>
2024-08-10 18:55:33 -0500

    Corregir funcionalidad de enrutamiento

--- lib/utils/router.dart
+++ lib/utils/router.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import 'package:ekmajstro_trejnisto/screens/screens.dart';
@@ -5,9 +7,16 @@ import 'package:ekmajstro_trejnisto/themes/themes.dart';
 
 const Widget ROUTER_INITIAL_PAGE = SplashscreenScreen();
 const String ROUTER_MAIN_ROUTE = '/app';
+const String ROUTER_POST_LIST_ROUTE = '/app/posts';
 
 Route<dynamic> mainRouter(RouteSettings settings) {
   if (settings.name == ROUTER_MAIN_ROUTE) {
+    return mainRouter(const RouteSettings(
+      name: ROUTER_POST_LIST_ROUTE,
+    ));
+  }
+
+  if (settings.name == ROUTER_POST_LIST_ROUTE) {
     return PageRouteBuilder(
       pageBuilder: (_, __, ___) => const PostListScreen(),
       transitionDuration:
