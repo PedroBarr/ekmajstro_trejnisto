16b74e6109e8bb36affd06dc81543e703f9ed1eb
Pedro <pebarreraa@udistrital.edu.co>
2025-05-05 01:31:50 -0500

    Corregir diseño de campo personalizado de imagen

--- lib/components/features/features_data/custom_image_field/custom_image_field_dialog.dart
+++ lib/components/features/features_data/custom_image_field/custom_image_field_dialog.dart
@@ -32,89 +32,115 @@ class CustomImageFieldDialog extends StatefulWidget {
 }
 
 class _CustomImageFieldDialog extends State<CustomImageFieldDialog> {
-  Widget getTitleWidget() {
-    if (widget.title != null) {
-      return Text(
-        widget.title!,
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          color: Theme.of(context).colorScheme.onSurface,
-          decoration: TextDecoration.none,
-          fontFamily: 'Roboto',
-        ),
-      );
-    } else {
-      return const SizedBox.shrink();
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     if (widget.mode == 'detail') {
       return Container(
-        constraints: BoxConstraints(
-          maxWidth: MediaQuery.of(context).size.width - 20,
-          maxHeight: MediaQuery.of(context).size.height - 50,
-        ),
         padding: const EdgeInsets.all(10.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.network(
-              widget.value!,
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(height: 10),
-            getTitleWidget(),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+                constraints: BoxConstraints(
+                  maxWidth: MediaQuery.of(context).size.width - 20,
+                  maxHeight: MediaQuery.of(context).size.height - 50,
+                ),
+                padding: const EdgeInsets.all(10.0),
+                child: Image.network(
+                  widget.value!,
+                  fit: BoxFit.fill,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Builder(
+                builder: (context) {
+                  if (widget.title != null) {
+                    return Text(
+                      widget.title!,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.onSurface,
+                        decoration: TextDecoration.none,
+                        fontFamily: 'Roboto',
+                      ),
+                    );
+                  } else {
+                    return const SizedBox.shrink();
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       );
     } else {
       return Container(
-        constraints: BoxConstraints(
-          maxWidth: MediaQuery.of(context).size.width - 20,
-          maxHeight: MediaQuery.of(context).size.height - 50,
-        ),
         padding: const EdgeInsets.all(10.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Builder(builder: (context) {
-              if (widget.title != null) {
-                return CustomTextFieldComponent(
-                  value: widget.title!,
-                  spacing: 10.0,
-                  font_size: 16,
-                  onConfirm: (value) {
-                    if (widget.onEditTitle != null) {
-                      widget.onEditTitle!(value);
-                    }
-                  },
-                  is_editable: widget.is_title_editable,
-                );
-              }
-              return const SizedBox.shrink();
-            }),
-            const SizedBox(height: 10),
-            Builder(
-              builder: (context) {
-                if (widget.value != null) {
-                  return Image.network(
-                    widget.value!,
-                    fit: BoxFit.cover,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Builder(builder: (context) {
+                if (widget.title != null) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                    padding: const EdgeInsets.all(10.0),
+                    alignment: Alignment.center,
+                    child: CustomTextFieldComponent(
+                      value: widget.title!,
+                      spacing: 10.0,
+                      font_size: 16,
+                      onConfirm: (value) {
+                        if (widget.onEditTitle != null) {
+                          widget.onEditTitle!(value);
+                        }
+                      },
+                      is_editable: widget.is_title_editable,
+                    ),
                   );
                 } else {
-                  return const Icon(
-                    Icons.library_add,
-                    color: Colors.grey,
-                  );
+                  return const SizedBox.shrink();
                 }
-              },
-            ),
-          ],
+              }),
+              const SizedBox(height: 10),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+                constraints: BoxConstraints(
+                  maxWidth: MediaQuery.of(context).size.width - 20,
+                  maxHeight: MediaQuery.of(context).size.height - 50,
+                ),
+                padding: const EdgeInsets.all(10.0),
+                child: Builder(
+                  builder: (context) {
+                    if (widget.value != null) {
+                      return Image.network(
+                        widget.value!,
+                        fit: BoxFit.cover,
+                      );
+                    } else {
+                      return Icon(
+                        Icons.library_add,
+                        color: Theme.of(context).colorScheme.onSurface,
+                      );
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
-        // child: TextField(),
       );
     }
   }
