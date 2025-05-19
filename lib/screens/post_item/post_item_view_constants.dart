f12182324b7dc2a469290abc45bc6be7762dca20
Pedro <pebarreraa@udistrital.edu.co>
2025-05-19 14:08:36 -0500

    Corregir constantes acordion vista de publicación

--- lib/screens/post_item/post_item_view_constants.dart
+++ lib/screens/post_item/post_item_view_constants.dart
@@ -2,7 +2,7 @@ const String post_cover_image_title = 'IMAGEN DE PORTADA';
 const String post_user_title = 'USUARIO';
 const String post_publish_date_title = 'FECHA DE PUBLICACIÓN';
 
-const String post_sections_title = 'SECCIONES';
-const String post_resources_title = 'RECURSOS';
-const String post_tags_title = 'ETIQUETAS';
-const String post_preview_title = 'PREVISUALIZACIÓN';
+const String post_sections_title = 'Secciones';
+const String post_resources_title = 'Recursos';
+const String post_tags_title = 'Etiquetas';
+const String post_preview_title = 'Previsualización';
