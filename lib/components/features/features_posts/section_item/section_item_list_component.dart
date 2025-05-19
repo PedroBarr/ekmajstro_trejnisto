171becd86667c5ac181e44ac1c0f2f4010c88c51
Pedro <pebarreraa@udistrital.edu.co>
2025-05-19 10:46:32 -0500

    Corregir diseño de lista de secciones de publicación (agregar icono de sección marcada)

--- lib/components/features/features_posts/section_item/section_item_list_component.dart
+++ lib/components/features/features_posts/section_item/section_item_list_component.dart
@@ -19,26 +19,56 @@ class _SectionItemListComponent extends State<SectionItemListComponent> {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Wrap(
-        direction: Axis.vertical,
-        spacing: 10.0,
-        crossAxisAlignment: WrapCrossAlignment.center,
-        children: widget.sections.map<Widget>((section) {
-          return Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5.0),
-              color: Theme.of(context).colorScheme.onPrimary,
-            ),
-            padding: const EdgeInsets.all(10.0),
-            width: MediaQuery.of(context).size.width - 20.0,
-            child: Text(
-              section.name,
-              style: TextStyle(
-                color: Colors.black,
+          direction: Axis.vertical,
+          spacing: 10.0,
+          crossAxisAlignment: WrapCrossAlignment.center,
+          children: [
+            [
+              const SizedBox(
+                height: 5,
               ),
-            ),
-          );
-        }).toList(),
-      ),
+            ],
+            widget.sections.map<Widget>((section) {
+              return Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5.0),
+                  color: Theme.of(context).colorScheme.onPrimary,
+                ),
+                padding: const EdgeInsets.all(10.0),
+                width: MediaQuery.of(context).size.width - 20.0,
+                child: Wrap(
+                  alignment: WrapAlignment.spaceBetween,
+                  direction: Axis.horizontal,
+                  children: [
+                    Text(
+                      section.name,
+                      style: TextStyle(
+                        color: Theme.of(context).scaffoldBackgroundColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Spacer(),
+                    Builder(
+                      builder: (BuildContext context) {
+                        return section.is_mark_one
+                            ? Icon(
+                                Icons.stars,
+                                color:
+                                    Theme.of(context).scaffoldBackgroundColor,
+                              )
+                            : SizedBox.shrink();
+                      },
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
+            [
+              const SizedBox(
+                height: 5,
+              ),
+            ]
+          ].expand((x) => x).toList()),
     );
   }
 }
