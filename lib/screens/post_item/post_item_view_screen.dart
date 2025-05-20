5f1817beb363baa9220a1aa6657f69b9a6dff054
Pedro <pebarreraa@udistrital.edu.co>
2025-05-20 15:59:40 -0500

    Remover renderización de prueba

--- lib/screens/post_item/post_item_view_screen.dart
+++ lib/screens/post_item/post_item_view_screen.dart
@@ -117,63 +117,6 @@ class _PostItemView extends State<PostItemView> {
     }
   }
 
-  // @override
-  Widget build2(BuildContext context) {
-    return Stack(
-      children: [
-        Scaffold(
-          appBar: AppBar(
-            leading: GestureDetector(
-              onTap: () => navigateToPostList(context),
-              child: iconNavPostList(Theme.of(context).colorScheme.onSurface),
-            ),
-            actions: [
-              Builder(builder: (context) {
-                if (_is_modified) {
-                  return GestureDetector(
-                    onTap: onSave,
-                    child: const Padding(
-                      padding: EdgeInsets.only(
-                        right: 10.0,
-                        left: 10.0,
-                      ),
-                      child: Icon(
-                        Icons.save_as_rounded,
-                      ),
-                    ),
-                  );
-                }
-                return Container();
-              }),
-            ],
-            title: Builder(
-              builder: (context) {
-                if (!_is_loading) {
-                  return CustomTextFieldComponent(
-                    value: _post.title,
-                    spacing: 10.0,
-                    font_size: 16,
-                    onConfirm: (value) => setPost(Post.POST_ATTR_TITTLE, value),
-                  );
-                } else {
-                  return const CircularProgressIndicator();
-                }
-              },
-            ),
-          ),
-          body: Column(
-            children: <Widget>[
-              Flexible(
-                child: Container(),
-              ),
-            ],
-          ),
-        ),
-        const FABEkmajstroComponent(),
-      ],
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Stack(
