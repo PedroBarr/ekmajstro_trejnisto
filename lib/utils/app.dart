7a6f94799fe042a287af69c7994a824eb0412ae9
Pedro <pebarreraa@udistrital.edu.co>
2025-05-20 04:18:01 -0500

    Corregir configuración de aplicativo frontal

--- lib/utils/app.dart
+++ lib/utils/app.dart
@@ -4,4 +4,4 @@ const String BACKEND_API = '$BACKEND_APP/api';
 const String BACKEND_ASSETS = '$BACKEND_APP/assets';
 
 const String FRONTEND_APP =
-    'https://pedrobarr.github.io/ekmajstro_pre_cion_frontend/';
+    'https://pedrobarr.github.io/ekmajstro_pre_cion_frontend';
