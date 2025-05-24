808334eedbcf211d62ffba84a4269edec53e2ac7
Pedro <pebarreraa@udistrital.edu.co>
2025-05-24 04:06:55 -0500

    Agregar diseño pantalla etiquetas

--- lib/screens/tag_list/tag_list_screen.dart
+++ lib/screens/tag_list/tag_list_screen.dart
@@ -26,21 +26,18 @@ class _TagListScreenState extends State<TagListScreen> {
     super.initState();
 
     loadTags();
-    debugPrint('TagListScreen initialized with post_id: ${widget.post_id}');
   }
 
   void loadTags() {
     getTags().then((tags) {
       setState(() {
         _tags = tags;
-        debugPrint('Tags loaded: $_tags');
       });
     }).whenComplete(() {
       if (widget.post_id != null) {
         getPostTagsList(widget.post_id!).then((tags) {
           setState(() {
             _selected_tags = tags;
-            debugPrint('Selected tags loaded: $_selected_tags');
           });
         });
       }
@@ -79,12 +76,115 @@ class _TagListScreenState extends State<TagListScreen> {
             ],
             title: const Text('Etiquetas'),
           ),
-          body: Center(
-            child: Text('Tag List Screen'),
+          body: Column(
+            children: [
+              const SizedBox(height: 10.0),
+              (widget.post_id != null && _selected_tags.isNotEmpty)
+                  ? Container(
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: GridView.builder(
+                        physics: const NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: _selected_tags.length,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 10,
+                          mainAxisSpacing: 10,
+                          childAspectRatio: 5.0,
+                        ),
+                        itemBuilder: (BuildContext context, int index) {
+                          final tag = _selected_tags[index];
+                          return GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                _selected_tags.remove(tag);
+                              });
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5.0),
+                                color: Theme.of(context).colorScheme.onPrimary,
+                              ),
+                              alignment: Alignment.center,
+                              child: Text(
+                                tag.name,
+                                style: TextStyle(
+                                  color:
+                                      Theme.of(context).scaffoldBackgroundColor,
+                                  fontSize: 16.0,
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    )
+                  : const SizedBox.shrink(),
+              Expanded(
+                child: (_tags.isNotEmpty
+                    ? Container(
+                        padding: const EdgeInsets.all(8.0),
+                        child: GridView.builder(
+                          physics: const AlwaysScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          itemCount: getMainTags().length,
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            crossAxisSpacing: 10,
+                            mainAxisSpacing: 10,
+                            childAspectRatio: 5.0,
+                          ),
+                          itemBuilder: (BuildContext context, int index) {
+                            final tag = getMainTags()[index];
+                            debugPrint('Building tag: ${tag.name}');
+                            return GestureDetector(
+                              onTap: () {},
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5.0),
+                                  color:
+                                      Theme.of(context).colorScheme.onSurface,
+                                ),
+                                alignment: Alignment.center,
+                                child: Text(
+                                  tag.name,
+                                  style: TextStyle(
+                                    color: Theme.of(context)
+                                        .scaffoldBackgroundColor,
+                                    fontSize: 16.0,
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      )
+                    : Center(
+                        child: CircularProgressIndicator(
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                      )),
+              ),
+            ],
           ),
         ),
         const FABEkmajstroComponent(),
       ],
     );
+  }
+
+  List<Tag> getMainTags() {
+    return _tags
+        .where((tag) => !_selected_tags
+            .map((tag) => tag.id.toString())
+            .toList()
+            .contains(tag.id.toString()))
+        .toList();
   }
 }
