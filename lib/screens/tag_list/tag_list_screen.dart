b290bd02e735893b73515025875eeac0e817fb3a
Pedro <pebarreraa@udistrital.edu.co>
2025-05-24 03:34:01 -0500

    Agregar funcionalidad almacenamiento interno etiquetas pantalla etiquetas

--- lib/screens/tag_list/tag_list_screen.dart
+++ lib/screens/tag_list/tag_list_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:ekmajstro_trejnisto/models/models.dart';
+import 'package:ekmajstro_trejnisto/utils/utils.dart';
+
 class TagListScreen extends StatefulWidget {
   final int? post_id;
 
@@ -13,6 +16,35 @@ class TagListScreen extends StatefulWidget {
 }
 
 class _TagListScreenState extends State<TagListScreen> {
+  List<Tag> _tags = [];
+  List<Tag> _selected_tags = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadTags();
+    debugPrint('TagListScreen initialized with post_id: ${widget.post_id}');
+  }
+
+  void loadTags() {
+    getTags().then((tags) {
+      setState(() {
+        _tags = tags;
+        debugPrint('Tags loaded: $_tags');
+      });
+    }).whenComplete(() {
+      if (widget.post_id != null) {
+        getPostTagsList(widget.post_id!).then((tags) {
+          setState(() {
+            _selected_tags = tags;
+            debugPrint('Selected tags loaded: $_selected_tags');
+          });
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
