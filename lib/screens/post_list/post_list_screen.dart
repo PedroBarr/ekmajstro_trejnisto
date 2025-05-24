26372bb014a740b178f7431c53fc87c8597be3dd
Pedro <pebarreraa@udistrital.edu.co>
2025-05-24 02:07:00 -0500

    Agregar funcionalidad recarga deslizante vista publicaciones

--- lib/screens/post_list/post_list_screen.dart
+++ lib/screens/post_list/post_list_screen.dart
@@ -53,71 +53,80 @@ class _PostListScreen extends State<PostListScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      body: Stack(
-        children: [
-          Center(
-            child: Column(
-              children: [
-                SearchBarComponent(
-                    hint_text: HINT_SEARCH_POST,
-                    onChanged: (String value) {
-                      setState(() {
-                        _searchText = value;
-                      });
-                    }),
-                const SizedBox(
-                  height: 10.0,
-                ),
-                Flexible(
-                  child: FutureBuilder(
-                    future: _posts,
-                    builder: (BuildContext context,
-                        AsyncSnapshot<List<PostItem>> snapshot) {
-                      if (_is_loading) {
-                        return const Center(child: CircularProgressIndicator());
-                      }
+    return RefreshSwipperComponent(
+      onRefresh: () {
+        loadPosts();
+      },
+      refreshing: _is_loading,
+      child: Scaffold(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        body: Stack(
+          children: [
+            Center(
+              child: Column(
+                children: [
+                  SearchBarComponent(
+                      hint_text: HINT_SEARCH_POST,
+                      onChanged: (String value) {
+                        setState(() {
+                          _searchText = value;
+                        });
+                      }),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  Flexible(
+                    child: FutureBuilder(
+                      future: _posts,
+                      builder: (BuildContext context,
+                          AsyncSnapshot<List<PostItem>> snapshot) {
+                        if (_is_loading) {
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        }
 
-                      if (snapshot.hasData) {
-                        return Expanded(
-                          child: Column(children: [
-                            Expanded(
-                              child: SingleChildScrollView(
-                                scrollDirection: Axis.vertical,
-                                child: Wrap(
-                                  direction: Axis.vertical,
-                                  spacing: 10.0,
-                                  crossAxisAlignment: WrapCrossAlignment.center,
-                                  children: [
-                                    snapshot.data!
-                                        .where((post) => post.title
-                                            .toLowerCase()
-                                            .contains(
-                                                _searchText.toLowerCase()))
-                                        .map((post) =>
-                                            PostItemComponent(post: post))
-                                        .toList(),
-                                    [
-                                      const AddPostItemComponent(),
-                                    ]
-                                  ].expand((x) => x).toList(),
+                        if (snapshot.hasData) {
+                          return Expanded(
+                            child: Column(children: [
+                              Expanded(
+                                child: SingleChildScrollView(
+                                  scrollDirection: Axis.vertical,
+                                  child: Wrap(
+                                    direction: Axis.vertical,
+                                    spacing: 10.0,
+                                    crossAxisAlignment:
+                                        WrapCrossAlignment.center,
+                                    children: [
+                                      snapshot.data!
+                                          .where((post) => post.title
+                                              .toLowerCase()
+                                              .contains(
+                                                  _searchText.toLowerCase()))
+                                          .map((post) =>
+                                              PostItemComponent(post: post))
+                                          .toList(),
+                                      [
+                                        const AddPostItemComponent(),
+                                      ]
+                                    ].expand((x) => x).toList(),
+                                  ),
                                 ),
                               ),
-                            ),
-                          ]),
-                        );
-                      } else {
-                        return const Center(child: CircularProgressIndicator());
-                      }
-                    },
+                            ]),
+                          );
+                        } else {
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        }
+                      },
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          const FABEkmajstroComponent(),
-        ],
+            const FABEkmajstroComponent(),
+          ],
+        ),
       ),
     );
   }
