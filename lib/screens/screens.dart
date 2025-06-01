6ead1ee8cad14918d06f39a9f6a25f3d24bae732
Pedro <pebarreraa@udistrital.edu.co>
2025-05-31 20:31:00 -0500

    Agregar exporatación de componentes de pantallas

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -3,3 +3,4 @@ export 'post_list/post_list.dart';
 export 'post_item/post_item.dart';
 export 'section_item/section_item.dart';
 export 'tag_list/tag_list.dart';
+export 'resource_list/resource_list.dart';
