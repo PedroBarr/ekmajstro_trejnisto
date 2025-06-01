ab0d081ce58440936e4be2654f1a69a1a30752a5
Pedro <pebarreraa@udistrital.edu.co>
2025-05-31 21:52:06 -0500

    Agregar constantes elemento previsualización

---
+++ lib/components/features/features_posts/preview_item/preview_item_constants.dart
@@ -0,0 +1,3 @@
+const String PREVIEW_COVER_IMAGE_TITLE = 'IMAGEN DE PREVISUALIZACIÓN';
+const String PREVIEW_WARNING_FIRST_SAVE =
+    'Recuerda que debes guardar la publicación para que la previsualización se vea reflejada.';
