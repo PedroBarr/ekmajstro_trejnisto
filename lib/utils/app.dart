6da538127ac2e0eba70649e5b4caf7029ef68e8a
Pedro <pebarreraa@udistrital.edu.co>
2025-06-08 15:20:12 -0500

    Agregar funcionalidad obtener simbolismo de Filebrowser

--- lib/utils/app.dart
+++ lib/utils/app.dart
@@ -5,3 +5,10 @@ const String BACKEND_ASSETS = '$BACKEND_APP/assets';
 
 const String FRONTEND_APP =
     'https://pedrobarr.github.io/ekmajstro_pre_cion_frontend';
+
+const String STORAGE_APP = 'https://filebrowser-production-26bd.up.railway.app';
+const String STORAGE_API = '$STORAGE_APP/api';
+
+String buildStorageSharedFilePublicUrl(String shared_id) {
+  return '$STORAGE_API/public/dl/$shared_id?inline=';
+}
--- lib/utils/filebrowser.dart
+++ lib/utils/filebrowser.dart
@@ -1,1 +1,32 @@
+import 'dart:async';
+import 'dart:convert';
 
+import 'package:http/http.dart' as http;
+
+import 'app.dart';
+import 'misc.dart';
+
+Future<String> getFBAToken(String username, String password) async {
+  try {
+    final response = await http.post(
+      Uri.parse('$STORAGE_API/login'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode(
+        {
+          'username': username,
+          'password': password,
+        },
+      ),
+    );
+
+    late String token = getRawText(response);
+
+    if (token.isEmpty) {
+      throw Exception('Invalid username or password');
+    }
+
+    return token;
+  } catch (e) {
+    throw Exception('Error connecting to File Browser API: $e');
+  }
+}
