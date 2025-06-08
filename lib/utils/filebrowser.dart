121406ec071846ebfc7c1a5a17c1e822674f8962
Pedro <pebarreraa@udistrital.edu.co>
2025-06-08 15:35:27 -0500

    Agregar funcionalidad obtener archivo compartido desde Filebrowser

--- lib/utils/filebrowser.dart
+++ lib/utils/filebrowser.dart
@@ -6,9 +6,11 @@ import 'package:http/http.dart' as http;
 import 'app.dart';
 import 'misc.dart';
 
+import 'package:ekmajstro_trejnisto/models/models.dart';
 import 'package:ekmajstro_trejnisto/config/config.dart';
 
 const String LOGIN_ENDPOINT = '/login';
+const String RECURSO_PUBLICO_ENDPOINT = '/public/share/';
 
 Future<String> getFBAToken(String username, String password) async {
   try {
@@ -32,6 +34,29 @@ Future<String> getFBAToken(String username, String password) async {
     return token;
   } catch (e) {
     throw Exception(ERROR_FILEBROWSER_LOGIN_FAILED);
+  }
+}
+
+Future<File> getResourceFromFB(String shared_id) async {
+  try {
+    final response = await http.get(
+      Uri.parse('$STORAGE_API$RECURSO_PUBLICO_ENDPOINT$shared_id'),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+
+    late dynamic body = getBody(response);
+    late File file = File.fromJson(body);
+
+    String mime_type = await getMymeTypeFromFB(shared_id);
+    file.mime_type = mime_type;
+
+    file.uri = buildStorageSharedFilePublicUrl(shared_id);
+
+    return file;
+  } catch (e) {
+    throw Exception(ERROR_FILEBROWSER_GET_SHARED_FILE);
   }
 }
 
